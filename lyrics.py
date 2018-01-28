017776143314bae1392b72875ed4cd6c73a8fc4f
Jasu Koponen <jasu.koponen@hotmail.com>
2018-01-29 00:04:13 +0200

    removed Curses dependency and added Windows support

--- lyrics.py
+++ lyrics.py
@@ -2,17 +2,20 @@
 """Prints the lyrics of the current Spotify song"""
 
 
-import curses
+import os
 import requests
 from sys import platform as pf
 from bs4 import BeautifulSoup as bs
+
 
 # Different operating systems store Spotify data differently
 if pf == "linux" or pf == "linux2":
     import dbus
 elif pf == "darwin":
     from subprocess import Popen, PIPE
-# elif pf == "win32" or "win64"
+elif pf == "win32" or "win64":
+    import pywintypes
+    import win32gui
 
 
 def get_info():
@@ -41,7 +44,9 @@ def get_info():
         artist = spotify[0]
         track = spotify[1]
 
-    # elif pf == "win32" or "win64":
+    elif pf == "win32" or "win64":
+        # On Windows, data is available through win32gui
+        artist, track = win32gui.GetWindowText(win32gui.FindWindow("SpotifyMainWindow", None)).split(" - ",1)
 
     return artist, track
 
@@ -65,8 +70,7 @@ def process_info(artist, track):
 
 def get_lyrics(artist, track):
     """Processes the URL http://lyrics.wikia.com/wiki/artist:track and returns the lyrics"""
-    lyrics = bs(requests.get("http://lyrics.wikia.com/wiki/" +
-                             artist + ":" + track).content, "lxml")
+    lyrics = bs(requests.get("http://lyrics.wikia.com/wiki/" + artist + ":" + track).content, "lxml")
 
     # Finds the wanted div from the lyrics
     lyrics = str(lyrics.find("div", {"class": "lyricbox"}))
@@ -84,92 +88,25 @@ def get_lyrics(artist, track):
     return lyrics
 
 
-def main(stdscr):
-    """Initializes the curses interface and prints the lyrics"""
-    stdscr.clear()
-    stdscr.refresh()
-    curses.curs_set(0)
-    max_y, max_x = stdscr.getmaxyx()
-
+def main():
+    """Prints the lyrics"""
     artist, track = get_info()
     artist, track = process_info(artist, track)
     lyrics = get_lyrics(artist, track)
 
     # Adds a title to the lyrics
-    title = [artist + " - " + track]
-    title = [" "] + title + [" "] + [" "]
+    lyrics = [" "] + [artist + " - " + track] + [" "] + lyrics;
 
-    longest = len(max(lyrics + title, key=len))
+    width = os.get_terminal_size().columns
+    longest = len(max(lyrics, key=len))
+    start = width - longest
 
-    pad_h = len(lyrics) + len(title) + 1
-    pad_w = longest
-
-    # Creates a pad according to the size of the lyrics
-    pad = curses.newpad(pad_h, pad_w)
-    pad.keypad(1)
-
-    pad_y = 0
-    pad_x = 0
-    center_x = round(max_x / 2 - longest / 2)
-
-    i = 0
-    for line in title:
-        pad.addstr(i, round((longest - len(line)) / 2), line)
-        i += 1
-
+    # Centers and prints the lyrics
     for line in lyrics:
-        pad.addstr(i, round((longest - len(line)) / 2), line)
-        i += 1
-
-    pad.refresh(pad_y, pad_x, 0, center_x, max_y - 1, max_x - 1)
-
-    while True:
-        # Compares the currently playing track to the displayed track
-        # If a different track is playing, updates the lyrics
-        pad.timeout(999)
-        if track != process_info(get_info()[0], get_info()[1])[1]:
-            main(stdscr)
-
-        pad_key = pad.getch()
-
-        if pad_key == curses.KEY_RESIZE:
-            stdscr.clear()
-            stdscr.refresh()
-            max_y, max_x = stdscr.getmaxyx()
-            center_x = round(max_x / 2 - longest / 2)
-            pad.refresh(pad_y, pad_x, 0, center_x, max_y - 1, max_x - 1)
-
-        # Movement keys
-        elif pad_key == curses.KEY_DOWN:
-            if pad_y + 1 + max_y > len(lyrics + title):
-                pad_y = pad_y
-            else:
-                pad_y += 1
-            pad.refresh(pad_y, pad_x, 0, center_x, max_y - 1, max_x - 1)
-
-        elif pad_key == curses.KEY_UP:
-            pad_y -= 1
-            if pad_y < 0:
-                pad_y = 0
-            pad.refresh(pad_y, pad_x, 0, center_x, max_y - 1, max_x - 1)
-
-        elif pad_key == curses.KEY_RIGHT:
-            if pad_x + 1 + max_x > len(max(lyrics, key=len)):
-                pad_x = pad_x
-            else:
-                pad_x += 1
-            pad.refresh(pad_y, pad_x, 0, center_x, max_y - 1, max_x - 1)
-
-        elif pad_key == curses.KEY_LEFT:
-            pad_x -= 1
-            if pad_x < 0:
-                pad_x = 0
-            pad.refresh(pad_y, pad_x, 0, center_x, max_y - 1, max_x - 1)
-
-        # F5 restarts the script
-        elif pad_key == curses.KEY_F5:
-            main(stdscr)
+        for i in range(0, int(start / 2) + int((longest - len(line)) / 2 )):
+            line = " " + line
+        print(line)
 
 
 if __name__ == "__main__":
-    curses.wrapper(main)
+    main()
